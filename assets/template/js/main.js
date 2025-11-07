43f05ab076200fb3ad1f4e16f3f88cec9d548c8b
Dmitriy <madaminov.d@gmail.com>
2025-11-07 18:03:29 +0500

    add category

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -1,8 +1,10 @@
 var window_width = document.body.clientWidth;
+var viewProducts = '';
 window.addEventListener(
   'resize',
   function (event) {
     window_width = document.body.clientWidth;
+    viewProducts(window_width);
   },
   true
 );
@@ -76,4 +78,78 @@ if (document.querySelector('.buttonCatalogToggle')) {
       sectionCatalogMenuToggle.classList.remove('open');
     }, 600);
   });
+}
+if (document.querySelector('.type-view-products')) {
+  const gridView = document.getElementById('grid-view');
+  const listView = document.getElementById('list-view');
+  const listViewCompact = document.getElementById('list-view-compact');
+  let productsList = document.querySelectorAll('.row-products .product-layout');
+  gridView.addEventListener('click', function (event) {
+    gridView.classList.add('active');
+    listView.classList.remove('active');
+    listViewCompact.classList.remove('active');
+    productsList.forEach((box) => {
+      box.className = '';
+      box.classList.add(
+        'product-layout',
+        'product-grid',
+        'col-12',
+        'col-md-4',
+        'col-lg-3',
+        'col-xl-1-of-5'
+      );
+    });
+    localStorage.setItem('display', 'grid');
+  });
+  listView.addEventListener('click', function (event) {
+    listView.classList.add('active');
+    gridView.classList.remove('active');
+    listViewCompact.classList.remove('active');
+    productsList.forEach((box) => {
+      box.className = '';
+      box.classList.add('product-layout', 'product-list', 'col-12');
+    });
+    localStorage.setItem('display', 'list');
+  });
+  listViewCompact.addEventListener('click', function (event) {
+    listViewCompact.classList.add('active');
+    gridView.classList.remove('active');
+    listView.classList.remove('active');
+    productsList.forEach((box) => {
+      box.className = '';
+      box.classList.add('product-layout', 'product-list-compact', 'col-12');
+    });
+    localStorage.setItem('display', 'list-compact');
+  });
+  viewProducts = (window_width) => {
+    if (window_width < 991) {
+      localStorage.setItem('display', 'grid');
+    }
+    if (localStorage.getItem('display') == 'list-compact') {
+      listViewCompact.dispatchEvent(new Event('click'));
+    } else if (localStorage.getItem('display') == 'list') {
+      listView.dispatchEvent(new Event('click'));
+    } else {
+      gridView.dispatchEvent(new Event('click'));
+    }
+  };
+}
+
+function plusCount(event) {
+  parent = event.parentElement;
+  value = parent.querySelector('input[name="quantity"]').value;
+  if (value > 0) {
+    parent.querySelector('input[name="quantity"]').value = parseInt(value) + 1;
+  } else {
+    parent.querySelector('input[name="quantity"]').value = 1;
+  }
+}
+function minusCount(event) {
+  parent = event.parentElement;
+  value = parent.querySelector('input[name="quantity"]').value;
+  if (value > 1) {
+    parent.querySelector('input[name="quantity"]').value = parseInt(value) - 1;
+  } else {
+    parent.querySelector('input[name="quantity"]').value = 1;
+  }
 }
